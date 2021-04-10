267da1c33312eb14e21fedbc4ee68580fca4f4b9
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2021-04-10 19:35:07 -0400

    updating

--- server.js
+++ server.js
@@ -53,6 +53,16 @@ mongoose.connect(
     app.use(flash()); //  for flash messages in session
 
 
+    if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'views', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'build', 'index.ejs'))
+  });
+
+}
+
+
     // CRUD ACCESS ROUTES
     require("./app/routes/main.js")(app, passport, db);
 
